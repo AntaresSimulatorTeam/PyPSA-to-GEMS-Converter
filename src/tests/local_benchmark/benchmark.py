8ddbc7369e5cd66ae83bd71d029cc1f88b3c3ab2
dusanparipovic <dusan.paripovic@rte-international.com>
2025-11-28 16:03:19 +0100

    style(code refactoring): code refactoring stage one benchmark

--- src/tests/local_benchmark/benchmark.py
+++ src/tests/local_benchmark/benchmark.py
@@ -19,34 +19,27 @@ current_dir = Path(__file__).resolve().parents[3]
 @pytest.mark.parametrize(
     "file_name, load_scaling, study_name",
     [
-        ("ac-dc-data_nl.nc", 1.0, "benchmark_study_ac_dc_data_nl"), #fast
-        #("base_s_11_elec_custom_1_nl.nc", 1.0, "benchmark_study_base_s_11_elec_custom_1_nl"), ValueError: Added data contains non-customized dimension names. This is not allowed when setting `force_dim_names` to True.
-        #("base_s_11_elec_custom_2_nl.nc", 1.0, "benchmark_study_base_s_11_elec_custom_2_nl"), Same -||-
-        #("base_s_11_elec_custom_3_nl.nc", 1.0, "benchmark_study_base_s_11_elec_custom_3_nl"), Same -||-
-        #("base_s_11_elec_nl.nc", 1.0, "benchmark_study_base_s_11_elec_nl"), Same -||-
-        ("base_s_20_elec_custom1_nl.nc", 1.0, "benchmark_study_base_s_20_elec_custom1_nl"), #Works not much fast but works
-        ("base_s_20_elec_nl.nc", 1.0, "benchmark_study_base_s_20_elec_nl"), #Works not much fast but works
-        ("base_s_4_elec.nc", 0.4, "benchmark_study_base_s_4_elec"), #fast
-        ("base_s_6_elec_lvopt__nl.nc", 1.0, "benchmark_study_base_s_6_elec_lvopt__nl"), #fast
-        ("base_s_6_elec_lvopt_.nc", 0.3, "benchmark_study_base_s_6_elec_lvopt_"), #fast
-        ("network_168_10_extendable_gen_nl.nc", 1.0, "benchmark_study_network_168_10_extendable_gen_nl"), #fast
-        ("network_168_10_s_nl.nc", 1.0, "benchmark_study_network_168_10_s_nl"), #fast
-        ("network_168_30_extendable_gen_nl.nc", 1.0, "benchmark_study_network_168_30_extendable_gen_nl"), #fast
-        ("network_168_30_nl.nc", 1.0, "benchmark_study_network_168_30_nl"), #fast
-        ("network_168_30_s_nl.nc", 1.0, "benchmark_study_network_168_30_s_nl"),#fast
-        ("network_168_60_extendable_gen_nl.nc", 1.0, "benchmark_study_network_168_60_extendable_gen_nl"), #1,5mb JUMP,fast
-        ("network_168_60_s_nl.nc", 1.0, "benchmark_study_network_168_60_s_nl"), #1,5mb JUMP
-        ("network_168_nl.nc", 1.0, "benchmark_study_network_168_nl"), #fast
-        ("network_1680_30_nl.nc", 1.0, "benchmark_study_network_1680_30_nl"), #3,7mb JUMP,fast
-        ("network_1680_30_s_nl.nc", 1.0, "benchmark_study_network_1680_30_s_nl"), #5,6mb JUMP,fast
-        ("network_336_10_s_nl.nc", 1.0, "benchmark_study_network_336_10_s_nl"), #fast
-        ("network_336_30_s_nl.nc", 1.0, "benchmark_study_network_336_30_s_nl"), #1,2mb jump,fast
-        ("network_336_60_s_nl.nc", 1.0, "benchmark_study_network_336_60_s_nl"), #2,6mb JUMP,fast
-        ("network_672_10_s_nl.nc", 1.0, "benchmark_study_network_672_10_s_nl"), #fast
-        #("network_8736_30_nl.nc", 1.0, "benchmark_study_network_8736_30_nl"), #22,3 mb JUMP 22minutes on my laptop 
-        #("simple.nc", 1.0, "benchmark_study_simple"),#fast TypeError: dtype 'carrier_carrier    object
-                                                            #E           carrier_carrier    object
-                                                            #E           dtype: object' not understood
+        ("ac-dc-data_nl.nc", 1.0, "benchmark_study_ac_dc_data_nl"),
+        ("base_s_20_elec_custom1_nl.nc", 1.0, "benchmark_study_base_s_20_elec_custom1_nl"),
+        ("base_s_20_elec_nl.nc", 1.0, "benchmark_study_base_s_20_elec_nl"),
+        ("base_s_4_elec.nc", 0.4, "benchmark_study_base_s_4_elec"),
+        ("base_s_6_elec_lvopt__nl.nc", 1.0, "benchmark_study_base_s_6_elec_lvopt__nl"),
+        ("base_s_6_elec_lvopt_.nc", 0.3, "benchmark_study_base_s_6_elec_lvopt_"),
+        ("network_168_10_extendable_gen_nl.nc", 1.0, "benchmark_study_network_168_10_extendable_gen_nl"),
+        ("network_168_10_s_nl.nc", 1.0, "benchmark_study_network_168_10_s_nl"),
+        ("network_168_30_extendable_gen_nl.nc", 1.0, "benchmark_study_network_168_30_extendable_gen_nl"),
+        ("network_168_30_nl.nc", 1.0, "benchmark_study_network_168_30_nl"),
+        ("network_168_30_s_nl.nc", 1.0, "benchmark_study_network_168_30_s_nl"),
+        ("network_168_60_extendable_gen_nl.nc", 1.0, "benchmark_study_network_168_60_extendable_gen_nl"),
+        ("network_168_60_s_nl.nc", 1.0, "benchmark_study_network_168_60_s_nl"),
+        ("network_168_nl.nc", 1.0, "benchmark_study_network_168_nl"),
+        ("network_1680_30_nl.nc", 1.0, "benchmark_study_network_1680_30_nl"),
+        ("network_1680_30_s_nl.nc", 1.0, "benchmark_study_network_1680_30_s_nl"),
+        ("network_336_10_s_nl.nc", 1.0, "benchmark_study_network_336_10_s_nl"),
+        ("network_336_30_s_nl.nc", 1.0, "benchmark_study_network_336_30_s_nl"),
+        ("network_336_60_s_nl.nc", 1.0, "benchmark_study_network_336_60_s_nl"),
+        ("network_672_10_s_nl.nc", 1.0, "benchmark_study_network_672_10_s_nl"),
+        ("network_8736_30_nl.nc", 1.0, "benchmark_study_network_8736_30_nl"),
     ],
 )
 def test_start_benchmark(file_name: str, load_scaling: float, study_name: str):
@@ -76,8 +69,6 @@ def test_start_benchmark(file_name: str, load_scaling: float, study_name: str):
     
     benchmark_data_frame.loc[0, "pypsa_version"] = network.pypsa_version
 
-    #read this from files we don't want to hardcode it
-    benchmark_data_frame.loc[0, "antares_solver_name"] = "highs"
 
     start_time_preprocessing = time.time()
     network = preprocess_network(network, True, True)
@@ -110,55 +101,49 @@ def test_start_benchmark(file_name: str, load_scaling: float, study_name: str):
         total_time_antares_modeler = time.time() - start_time_antares_modeler
         benchmark_data_frame.loc[0, "antares_modeler__total_time"] = total_time_antares_modeler
 
-
-        output_dir = study_dir / "systems" / "output"
-        result_file = [f for f in output_dir.iterdir() if f.is_file() and f.name.startswith("simulation_table")]
-
-
-        objective_value = None
-        if result_file:
-            # -1 because we want the last result file
-            if result_file[-1].suffix == ".csv":
-                df = pd.read_csv(result_file[-1])
-                objective_value = float(df.iloc[-1, -2])
-            elif result_file[-1].suffix == ".tsv":
-                df = pd.read_csv(result_file[-1], sep="\t")
-                objective_value = float(df.iloc[-1, -2])
-        benchmark_data_frame.loc[0, "antares_objective_value"] = objective_value
-
-
-        mps_files = [f for f in output_dir.iterdir() if f.is_file() and f.name.endswith(".mps") and f.name != "master.mps"]
-        if mps_files:
-            mps_file = mps_files[0]
-            with open(mps_file, 'r') as f:
-                lines = f.readlines()
-                if len(lines) > 3 and 'Constraints' in lines[3]:
-                    constraints = int(lines[3].split(':')[-1].strip())
-                    benchmark_data_frame.loc[0, "number_of_constraints_antares"] = constraints
-                if len(lines) > 4 and 'Variables' in lines[4]:
-                    variables = int(lines[4].split(':')[-1].strip())
-                    benchmark_data_frame.loc[0, "number_of_variables_antares"] = variables
-        print(f"number of constraints: {benchmark_data_frame.loc[0, 'number_of_constraints_antares']}")
-        print(f"number of variables: {benchmark_data_frame.loc[0, 'number_of_variables_antares']}")
-
-        
     except subprocess.CalledProcessError as e:
-        raise Exception(f"Antares modeler failed with error: {e}")
+        raise RuntimeError(f"Antares modeler failed with error: {e}")
+
+    output_dir = study_dir / "systems" / "output"
+    result_file = [f for f in output_dir.iterdir() if f.is_file() and f.name.startswith("simulation_table")]
+
+
+    objective_value = None
+    if result_file:
+        if result_file[-1].suffix == ".csv":
+            df = pd.read_csv(result_file[-1])
+            objective_value = float(df.iloc[-1, -2])
+        elif result_file[-1].suffix == ".tsv":
+            df = pd.read_csv(result_file[-1], sep="\t")
+            objective_value = float(df.iloc[-1, -2])
+    benchmark_data_frame.loc[0, "antares_objective_value"] = objective_value
+
+
+    mps_files = [f for f in output_dir.iterdir() if f.is_file() and f.name.endswith(".mps") and f.name != "master.mps"]
+    if mps_files:
+        mps_file = mps_files[0]
+        with open(mps_file, 'r') as f:
+            lines = f.readlines()
+            if len(lines) > 3 and 'Constraints' in lines[3]:
+                constraints = int(lines[3].split(':')[-1].strip())
+                benchmark_data_frame.loc[0, "number_of_constraints_antares"] = constraints
+            if len(lines) > 4 and 'Variables' in lines[4]:
+                variables = int(lines[4].split(':')[-1].strip())
+                benchmark_data_frame.loc[0, "number_of_variables_antares"] = variables
+
 
     parameters_yml_path = current_dir / "tmp" / study_name / "systems" / "parameters.yml"
     with Path(parameters_yml_path).open() as f:
         parameters_yml = yaml.safe_load(f)   
         benchmark_data_frame.loc[0, "antares_solver_parameters"] = parameters_yml["solver-parameters"]
+        benchmark_data_frame.loc[0, "antares_solver_name"] = parameters_yml["solver"]
 
     #make pypsa optimization problem equations,constraints,variables
     start_time_build_optimization_problem = time.time()
-    model = create_model(network) 
+    create_model(network) 
     build_optimization_problem_time_pypsa = time.time() - start_time_build_optimization_problem
 
     benchmark_data_frame.loc[0, "build_optimization_problem_time_pypsa"] = build_optimization_problem_time_pypsa
-    #benchmark_data_frame.loc[0, "number_of_constraints_pypsa"] = len(model.constraints) single constraint
-    #benchmark_data_frame.loc[0, "number_of_variables_pypsa"] = len(model.variables) single variable
-
     
     #solve pypsa optimization problem
     total_time_start = time.time()
@@ -180,11 +165,9 @@ def test_start_benchmark(file_name: str, load_scaling: float, study_name: str):
     number_of_variables = solver.getNumCol()
     benchmark_data_frame.loc[0, "number_of_variables_pypsa"] = number_of_variables
 
-
     
-    #this is approximately the time spent solving the problem,because we use create_model from linopy,pypsa probably has internal logic inside .optimize()
+    #this is approximately the time spent solving the problem,because we use create_model from linopy,pypsa has internal logic inside .optimize()
     #but if we call network.optimize.solve_model() we cannot get the number of constraints and variables
-    #to skip 2x calling of network.optimize() ,network.optimize.solve_model() we use network.optimize()
     benchmark_data_frame.loc[0, "pypsa_optimization_time"] = total_time - build_optimization_problem_time_pypsa
     benchmark_data_frame.loc[0, "total_time_pypsa"] = total_time
 
@@ -193,11 +176,6 @@ def test_start_benchmark(file_name: str, load_scaling: float, study_name: str):
     benchmark_data_frame.loc[0, "solver_version_pypsa"] = network.model.solver_model.version()
     benchmark_data_frame.loc[0, "pypsa_objective"] = network.objective + network.objective_constant
 
-    antares_objective_value = benchmark_data_frame.loc[0, "antares_objective_value"]
-    pypsa_objective = benchmark_data_frame.loc[0, "pypsa_objective"]
-
-    print(f"antares_objective_value: {antares_objective_value}")
-    print(f"pypsa_objective: {pypsa_objective}")
     
     shutil.rmtree(current_dir / "tmp" / study_name)
 
@@ -207,12 +185,11 @@ def test_start_benchmark(file_name: str, load_scaling: float, study_name: str):
     results_dir.mkdir(parents=True, exist_ok=True)
     combined_results_file = results_dir / "all_studies_results.csv"
     
-    # Check if file exists to determine if we need headers
     file_exists = combined_results_file.exists()
     benchmark_data_frame.to_csv(
         combined_results_file, 
-        mode='a',  # append mode
-        header=not file_exists,  # write header only if file doesn't exist
+        mode='a',
+        header=not file_exists,  
         index=False
     )
     logger.info(f"Appended benchmark results to {combined_results_file}")
