63d773f497e70584c18e2e2ee0afe08b15a68623
dusanparipovic <dusan.paripovic@rte-international.com>
2025-12-25 13:29:45 +0100

    feat(test): add one more scenarized test withot time series values

--- tests/unit_tests/test_pypsa_preprocessor.py
+++ tests/unit_tests/test_pypsa_preprocessor.py
@@ -16,7 +16,7 @@ from src.pypsa_preprocessor import PyPSAPreprocessor
 from src.utils import StudyType
 
 
-def test_rename_buses_2_stage_stochastic_optimization():
+def test_rename_buses_2_stage_stochastic_optimization() -> None:
     # Create a simple network with 10 time steps
     network = Network(name="Simple_Network", snapshots=[i for i in range(10)])
 
@@ -70,6 +70,65 @@ def test_rename_buses_2_stage_stochastic_optimization():
     if hasattr(network, "has_scenarios"):
         assert network.has_scenarios
 
-    PyPSAPreprocessor(network, StudyType.TWO_STAGE_STOCHASTIC).network_preprocessing() #call preprocessor
+    PyPSAPreprocessor(network, StudyType.TWO_STAGE_STOCHASTIC).network_preprocessing()  # call preprocessor
+
+    network.optimize()  # check if pypsa can optimize the network,if everything is correctly renamed
+
+
+def test_rename_buses_2_stage_stochastic_optimization_two() -> None:
+    # Create a simple network with 10 time steps
+    network = Network(name="Simple_Network", snapshots=[i for i in range(10)])
+
+    # Add a bus
+    # Add the carrier before creating the bus
+    network.add("Carrier", "carrier", co2_emissions=0)
+    network.add("Bus", "bus1", v_nom=1, carrier="carrier")
+
+    # Add a load
+    network.add("Load", "load1", bus="bus1", p_set=100, q_set=0)
+
+    # Add generators with initial p_max_pu values
+    network.add(
+        "Generator",
+        "gen1",
+        bus="bus1",  # intentional
+        p_nom_extendable=False,
+        marginal_cost=50,  # €/MWh
+        p_nom=200,  # MW
+        p_max_pu=0.9,  # Base value
+    )
+
+    network.add(
+        "Generator",
+        "gen2",
+        bus="bus1",  # intentional
+        p_nom_extendable=False,
+        marginal_cost=10,  # €/MWh
+        p_nom=100,  # MW
+        p_min_pu=0.0,
+        p_max_pu=0.7,
+    )
+
+    # Define scenarios with probabilities (these will be used as coefficients)
+    # Have that on mind this number isn't multiplied with numbers in p_max_pu
+    scenarios = {
+        "low": 0.3,
+        "medium": 0.5,
+        "high": 0.2,
+    }
+
+    # test do we have scenarios (check if attribute exists)
+    if hasattr(network, "has_scenarios"):
+        assert not network.has_scenarios
+
+    # Set scenarios in the network
+    network.set_scenarios(scenarios)
+
+    # test do we have scenarios
+    assert hasattr(network, "has_scenarios")
+    if hasattr(network, "has_scenarios"):
+        assert network.has_scenarios
+
+    PyPSAPreprocessor(network, StudyType.TWO_STAGE_STOCHASTIC).network_preprocessing()  # call preprocessor
 
     network.optimize()  # check if pypsa can optimize the network,if everything is correctly renamed
